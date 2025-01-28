cebe4d93e89cca5ba06998c4ec7250e68bfa51d3
Clément MOURGUE <cmourgue@iutbayonne.univ-pau.fr>
2025-01-28 15:35:45 +0100

    Test ajout lamp dans Hero

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,7 +1,10 @@
+"use client";
 import { ComponentPropsWithoutRef } from "react";
 import { Section } from "./Section";
 import { LaravelIcon } from "./icons/LaravelIcon";
 import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+/* import { LampContainer } from "@/components/ui/lamp"; */
 
 export const Code = ({ className, ...props }: ComponentPropsWithoutRef<"span">) => {
     return (
@@ -14,7 +17,21 @@ export const Hero = () => {
     return (
         <Section>
             <h2 className="font-caption text-4xl">Clément MOURGUE</h2>
-            <h4 className="font-caption text-2xl mb-3">Student at IUT de Bayonne et du Pays Basque</h4>
+            {/* <LampContainer> */}
+                <motion.h4
+                    initial={{ opacity: 0.5, y: 100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{
+                    delay: 0.3,
+                    duration: 0.8,
+                    ease: "easeInOut",
+                    }}
+                    className="font-caption text-2xl mb-3 min-w-screen"
+                >
+                    Student at IUT de Bayonne et du Pays Basque
+                </motion.h4>
+            {/* </LampContainer> */}
+            {/* <h4 className="font-caption text-2xl mb-3">Student at IUT de Bayonne et du Pays Basque</h4> */}
             <p>
                 Écrire du <Code><LaravelIcon size={16}></LaravelIcon>code</Code>
             </p>
